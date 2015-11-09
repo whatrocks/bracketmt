6c17230c749702de6c3b51b502a4e1264eef2dda
Charles Harrington <charles.harrington@gmail.com>
2015-11-09 12:09:24 -0800

    Client: setting up routes

--- client/app/app.js
+++ client/app/app.js
@@ -1,4 +1,13 @@
 angular.module('bracketmt', [
   'ui.router',
   'ngMaterial'
-]);
+])
+.config(function ($stateProvider, $urlRouterProvider, $httpProvider) {
+  $stateProvider
+    .state('nav', {
+      url: '/',
+      templateUrl: 'app/nav/nav.html'
+    });
+
+  $urlRouterProvider.otherwise('/');
+});
