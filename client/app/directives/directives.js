5779a7093dd0766bc604362944efaa62c9e1901d
Charles Harrington <charles.harrington@gmail.com>
2015-11-10 17:25:01 -0800

    d3 balls are moving around

--- client/app/directives/directives.js
+++ client/app/directives/directives.js
@@ -4,10 +4,10 @@ angular.module('bracketmt.directives', [])
     return {
       template: '<div></div>',
       restrict: 'E',
-      replace: false,
+      replace: true,
       scope: { data: '='},
       link: function postLink(scope, element, attrs) {
-        var w = 1000;
+        var w = window.innerWidth;
         var h = 600;
 
         var svg = d3.select(element[0]).append('svg')
@@ -15,20 +15,25 @@ angular.module('bracketmt.directives', [])
           .attr('height', h);
 
         scope.$watch('data', function(data) {
+          
           if (data) {
-            var escala = function(m) {
-              return d3.scale
-                .linear()
-                .domain([d3.min(data), d3.max(data)])
-                .range([10, m - 50]);
-            };
 
-            var ancho = escala(w);
-            var alto = escala(h);
-            var radio = d3.scale
-              .linear()
-              .domain([d3.min(data), d3.max(data)])
-              .range([10, 50]);
+            var randomX = function() { return Math.random () * w;};
+            var randomY = function() { return Math.random () * h;};
+
+            // var escala = function(m) {
+            //   return d3.scale
+            //     .linear()
+            //     .domain([d3.min(data), d3.max(data)])
+            //     .range([10, m - 50]);
+            // };
+
+            // var ancho = escala(w);
+            // var alto = escala(h);
+            // var radio = d3.scale
+            //   .linear()
+            //   .domain([d3.min(data), d3.max(data)])
+            //   .range([10, 50]);
 
             var color = function(d) {
               return d3.hsl(d*10,1,0.5);
@@ -36,28 +41,36 @@ angular.module('bracketmt.directives', [])
 
             var circle_set = function(c) {
               c
-                .attr('r', radio)
-                .attr('cx', ancho)
-                .attr('cy', alto)
+                .attr('r', 25)
+                .attr('cx', randomX)
+                .attr('cy', randomY)
                 .style('fill', color);
             };
 
             var circles = svg.selectAll('circle');
             var sel = circles.data(data);
+            
+            var moving = function() {
+              sel
+                .transition()
+                .ease('bounce')
+                .duration(1000)
+                .call(circle_set);
 
-            sel
-              .transition()
-              .ease('bounce')
-              .duration(500)
-              .call(circle_set);
+              sel.enter()
+                .append('circle')
+                .call(circle_set);
 
-            sel.enter()
-              .append('circle')
-              .call(circle_set);
+              sel.exit()
+                .remove();
+            };
+            
+            setInterval(function(){
+              moving();
+            }.bind(this), 1000);
 
-            sel.exit()
-              .remove();
           }
+
         });
       }
     };
